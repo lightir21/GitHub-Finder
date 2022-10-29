3b7c70edcc3c059610d56b739f2c6a621fb2c258
meir <lightir21@gmail.com>
2022-10-29 23:52:06 +0300

    add Default value and type to title
    Navbar.jsx:
    	-import PropTypes
    	-create nav element
    	-set in default props title: 'Github Finder'
    	-set in propTypes title :PropeTypes.string

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      Navbar
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Navbar;
