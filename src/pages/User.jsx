09808916baa6fe53e11a88fa5f9bc8b049cfce5a
meir <lightir21@gmail.com>
2022-11-06 10:24:14 +0200

    Get user into User component
    User.jsx:
    	-import useEffect, useContext, GithubContext, useParams
    	-destructure from useContext with Githubcontext passed in {getUser, user}
    	-create a params constant and invoke useParams on it
    	-create a useEffect with an empty dependecie array\
    	-set getUser function with params.login as a argument

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useEffect, useContext } from "react";
+import GithubContext from "../context/github/GithubContext";
+import { useParams } from "react-router-dom";
 
 const User = () => {
-  return <div>User</div>;
+  const { getUser, user } = useContext(GithubContext);
+
+  const params = useParams();
+
+  useEffect(() => {
+    getUser(params.login);
+  }, []);
+
+  return <div>{user.login}</div>;
 };
 
 export default User;
