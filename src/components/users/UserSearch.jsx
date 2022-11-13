e3eb3d11673807986fcd8fa368412b5c81a9c9e0
meir <lightir21@gmail.com>
2022-11-13 09:55:08 +0200

    Moving getUser and getUserRepos to actions
    -cut and paste getUser and getUserRepos from GithubContext and paste inside of GithubActions
    -delete the functions from the provider as well
    
    GithubActions.js:
    	-export both functions
    	-delete setLoading from the functions
    	-instead of dispatch just return data
    
    User.jsx:
    	-import getUser and getUserRepos
    	-useEffect:
    		-delete the function calls inside of the useEffect
    		-dispatch type "SET_LOADING"
    		-create a function called getUserData make it async invoke in the end of the useEffect // we cant make useEffect async so we create a function inside of it
    			-create a variable called userData and await getUser bad pass in params.login
    			-dispatch with type of "GET_USER" and the payload of userData
    			-create a variable userRepoData and await getUserRepos and pass in params.login
    			-dispatch with type of "GET_REPOS" and pass payload of userRepoData
    			-set dispatch and params.login inside of useEffect dependencie array

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -6,7 +6,7 @@ import { searchUsers } from "../../context/github/GithubActions";
 const UserSearch = () => {
   const [text, setText] = useState("");
 
-  const { users, dispatch, clearUsers } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
@@ -50,7 +50,10 @@ const UserSearch = () => {
       </div>
       {users.length > 0 && (
         <div>
-          <button className="btn btn-ghost btn-lg" onClick={clearUsers}>
+          <button
+            className="btn btn-ghost btn-lg"
+            onClick={() => dispatch({ type: "CLEAR_USERS" })}
+          >
             Clear
           </button>
         </div>
